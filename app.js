a3072efa509c06bd6b79c1736b7fb7b56963677d
boxulavm <you@example.com>
2019-06-24 14:19:45 +0200

    'progress'

--- app.js
+++ app.js
@@ -23,4 +23,10 @@ $(document).ready(function() {
     });
   });
 
-  
+  // Parallax
+  const parallax = document.getElementById('parallax');
+
+  window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+})
